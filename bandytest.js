d544b0c4cfcfbf18337a516e9cb831283e07317a
Gagarin-kun <49089219+Gagarin-kun@users.noreply.github.com>
2019-05-21 21:36:46 +0500

    Добавили подгрузку из внешнего файла с данными

--- bandytest.js
+++ bandytest.js
@@ -1,22 +1,22 @@
-var numbers = [4, 0, 3, 19, 492, -10, 1];                            // Получаем массив с числами
-console.log(numbers);                         // Просматриваем 
-if (numbers.length <= 1) {
+var numbers = require('./testmodul1');                            // Получаем массив с числами
+console.log(numbers.num);                         // Просматриваем 
+if (numbers.num.length <= 1) {
    console.log('Недастаточно чисел, для осуществления операции!')  // Проверяем, достаточно, ли чисел в массиве
 } else {
 
-for (var i = 0; i <= numbers.length - 2; i++) {         // Выстраиваем значения в массиве по возростанию
-  var minValue = numbers[i];
+for (var i = 0; i <= numbers.num.length - 2; i++) {         // Выстраиваем значения в массиве по возростанию
+  var minValue = numbers.num[i];
 
-  for (var j = i + 1; j <= numbers.length - 1; j++) {
-    if (numbers[j] < minValue) {
-      minValue = numbers[j];
-      var swap = numbers[i];
-      numbers[i] = minValue;
-      numbers[j] = swap;
+  for (var j = i + 1; j <= numbers.num.length - 1; j++) {
+    if (numbers.num[j] < minValue) {
+      minValue = numbers.num[j];
+      var swap = numbers.num[i];
+      numbers.num[i] = minValue;
+      numbers.num[j] = swap;
     }
   }
 }
-console.log(numbers);                                    // Смотрим на то что получилось
+console.log(numbers.num);                                    // Смотрим на то что получилось
 
-console.log('Сумма минимальных значений массива:' + (numbers[0] + numbers[1])); // Суммируем два минимальных значения и выводим результат
+console.log('Сумма минимальных значений массива:' + (numbers.num[0] + numbers.num[1])); // Суммируем два минимальных значения и выводим результат
 }
