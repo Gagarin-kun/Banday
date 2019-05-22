9bca0c3f205052afc49f65a531238e9db88c77d4
Gagarin-kun <49089219+Gagarin-kun@users.noreply.github.com>
2019-05-22 16:19:54 +0500

    Просто генератор рандомных чисел

--- testmodul1.js
+++ testmodul1.js
@@ -1,3 +1,13 @@
-var numbers = [1, 3, 8, -9, 33]
+// Генератор случайного числового массива
 
-exports.num = numbers;
+var numbers = [];
+var randomNum
+var arrLength = 10   // Задаем длинну массива
+var numScale = 1000   // Задаем максимальный размер числа
+
+for (var i = 0; i <= arrLength; i++ ){                         //Генерируем случайные числа
+randomNum = Math.floor(Math.random() * (numScale + 1)); 
+numbers.push(randomNum)
+}
+
+exports.num = numbers;     //Готовим для отправки 
